f83bd4a6e0ed74cea761abcebeec64ca27b36dd5
Santiago Dimaren <sdimaren1@gmail.com>
2024-02-04 21:59:14 -0500

    visual editing, allow for posts to render in client

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 // ./app/page.tsx
 
 import { SanityDocument } from "next-sanity";
+import { draftMode } from "next/headers";
 
 import Posts from "@/components/Posts";
+import PostsPreview from "@/components/PostPreview";
 import { loadQuery } from "@/sanity/lib/store";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Page() {
-  const initial = await loadQuery<SanityDocument[]>(POSTS_QUERY);
+  const initial: any = await loadQuery<SanityDocument[]>(POSTS_QUERY, {}, {
+    perspective: draftMode().isEnabled ? "previewDrafts" : "published",
+  });
+  
 
-  return <Posts posts={initial.data} />
+  return draftMode().isEnabled ? (
+    <PostsPreview initial={initial} params={[]}/>
+  ) : (
+    <Posts posts={initial.data} />
+  )
 }
--- sanity.config.ts
+++ sanity.config.ts
@@ -5,6 +5,8 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
+import { presentationTool } from 'sanity/presentation'
+import { locate } from '@/sanity/presentation/locate'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
@@ -17,9 +19,17 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
+    locate,
     structureTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
+    presentationTool({
+      previewUrl: {
+        draftMode: {
+          enable: '/api/draft',
+        },
+      },
+    }),
   ],
 })
